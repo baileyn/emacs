3e8d4b71a4ab0ee7b037bf754e6ce3ed0661ac4e
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-10-03 15:01:53 -0600

    feat(config): add some more packages

--- init.el
+++ init.el
@@ -1,11 +1,18 @@
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(tooltip-mode -t)
+(tooltip-mode t)
 (menu-bar-mode -1)
 (setq visible-bell t)
 
-(load-theme 'wombat t)
+(column-number-mode)
+
+(global-display-line-numbers-mode t)
+(setq display-line-numbers 'relative)
+
+(dolist (mode '(term-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 110)
 (set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 110)
@@ -25,10 +32,28 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package which-key
+  :diminish
+  :init (which-key-mode)
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
 (use-package diminish)
 
 (use-package ivy
   :diminish
+  :init
+  (ivy-mode 1)
   :bind (("C-s" . swiper)
 	 :map ivy-minibuffer-map
 	 ("TAB" . ivy-alt-done)
@@ -39,8 +64,37 @@
 	 ("C-d" . ivy-switch-buffer-kill)
 	 :map ivy-reverse-i-search-map
 	 ("C-k" . ivy-previous-line)
-	 ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+	 ("C-d" . ivy-reverse-i-search-kill)))
 
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
 
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package doom-themes)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons which-key use-package rainbow-delimiters ivy-rich helpful doom-themes doom-modeline diminish counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
