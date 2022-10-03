2b9f0a4fd24d6e5048815a126a1273d181202db9
Nicholas Bailey <nicholas.j.bailey91@gmail.com>
2022-10-03 15:38:21 -0600

    feat(config): add evil mode

--- init.el
+++ init.el
@@ -89,3 +89,16 @@
   :custom ((doom-modeline-height 15)))
 
 (load-theme 'doom-dark+ t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(undo-tree evil general all-the-icons which-key use-package rainbow-delimiters ivy-rich helpful doom-themes doom-modeline diminish counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
